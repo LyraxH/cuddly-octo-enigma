8d5dcf4acf29a2de8b6e541441158495002fa164
LyraxH <71412637+LyraxH@users.noreply.github.com>
2026-06-10 15:21:28 +1200

    install script updates

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,58 +1,106 @@
 #!/bin/bash
-echo "running balls install script"
+echo "running bastard install script"
 
-echo "remove kde!!!"
-sudo pacman -Rncs --noconfirm dolphin
+echo "Purging all KDE shitterware"
+KDE_Shitters=(
+    "dolphin"
+    "kwallet"
+    "kwallet5"
+    "kwallet6"
+    "kwayland"
+    "kauth"
+    "kconfig"
+)
 
-echo "updating system"
+for pkg in "${KDE_Shitters[@]}"; do
+    if pacman -Qi "$pkg" &>/dev/null; then
+        echo "Removing $pkg..."
+        sudo pacman -Rns --noconfirm "$pkg" 2>/dev/null || true
+    fi
+done
+
+echo "Cleaning up leftover KDE framework dependencies..."
+sudo pacman -Rns $(pacman -Qdtq) 2>/dev/null || true
+
+echo "Updating System"
 sudo pacman -Syu --noconfirm
 
-PACKAGES=(
-	#essentials
-	"rofi"
-	"waybar"
-	"hyprpolkitagent"
-	"hyprpaper"
-	"hyprlock"
-	"hyprshot-gui"
-	"power-profiles-daemon"
-	"feh"
-	"noto-fonts"
-	"noto-fonts-cjk"
-	"noto-fonts-emoji"
-	"otf-font-awesome"
-	"woff2-font-awesome"
-	"ttf-jetbrains-maple-mono-nf-xx-xx"
-	"ttf-jetbrains-mono-git"
-	"ttf-breeze-sans"
-	"pywal"
-	"ffmpegthumbnailer"
-	"brightnessctl"
-	"playerctl"
-	"pipewire"
-	"pipewire-pulse"
-	"pipewire-audio"
-	"pipewire-session-manager"
-	"wireplumber"
-	"jdk-openjdk"
-	"sof-firmware"
-	"alsa-ucm-conf"
+Packages=(
+    # essentials
+    "7zip"
+    "antigravity-ide"
+    "bashtop"
+    "blender-bin"
+    "blueman"
+    "bluetui"
+    "brave-bin"
+    "brightnessctl"
+    "catppuccin-cursors-mocha"
+    "catppuccin-gtk-theme-mocha"
+    "etcher-bin"
+    "fastfetch"
+    "feh"
+    "grim"
+    "gnome-keyring"
+    "hyprlock"
+    "hyprpaper"
+    "hyprpolkitagent"
+    "hyprshot"
+    "hyprpicker"
+    "libsecret"
+    "ncdu"
+    "nemo"
+    "nemo-file-roller"
+    "neovim"
+    "otf-font-awesome"
+    "pavucontrol"
+    "pipewire"
+    "pipewire-audio"
+    "pipewire-pulse"
+    "playerctl"
+    "power-profiles-daemon"
+    "rofi"
+    "swaync"
+    "tailscale"
+    "ttf-jetbrains-maple-mono-nf-xx-xx"
+    "unityhub"
+    "waybar"
+    "wev"
+    "wireplumber"
+    "wireguard-tools"
+    "wl-clipboard"
+    "woff2-font-awesome"
+    "yazi"
+    "yt-dlp"
+    
 
+    # for funs
+    "asciiquarium"
+    "cava"
+    "cbonsai-git"
+    "cmatrix"
+    "pipes.sh"
+    "pinta"
+    "rpi-imager"
+    "spotify"
+    "spicetify-cli"
 
-	#my apps
-	"code"
-	"yazi"
-	"discord"
-	"spotify"
-	"brave-bin"
-	"notesnook-bin"
-	"nemo"
-	"spicetify-cli"
-	"fastfetch"
-	"pipes.sh"
 )
 
 echo "installing packages"
-paru -S --noconfirm "${PACKAGES[@]}"
+paru -S --noconfirm "${Packages[@]}"
 
-echo "done."
+echo "packages installed"
+echo "initiating rice"
+
+echo "cloning from repo"
+cd
+git clone https://github.com/lyraxh/cuddly-octo-enigma
+
+echo "creating config"
+mkdir -p ~/.config
+cd ~/cuddly-octo-enigma
+cp -r hypr kitty rofi waybar yazi ~/.config/
+rm -rf ~/cuddly-octo-enigma
+
+echo "done"
